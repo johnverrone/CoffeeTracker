851619775397dfba0e4ae2ad2d6f3946e4b92b44
John Verrone <john_verrone@homedepot.com>
2017-08-15 12:36:29 -0400

    fixed bug with adding coffee

--- src/components/CoffeeInput.js
+++ src/components/CoffeeInput.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Card, Form, TextArea } from 'semantic-ui-react'
 
 const CoffeeInput = ({addCoffee}) => {
-  let input;
+  let name, roaster, description;
 
   return (
     <Form>
@@ -10,21 +10,21 @@ const CoffeeInput = ({addCoffee}) => {
         <Card.Content>
           <Form.Field>
             <label>Name</label>
-            <input type="text" name="name" ref={node => {input = node}} />
+            <input type="text" name="name" ref={node => {name = node}} />
           </Form.Field>
           <Form.Field>
             <label>Coffee Roaster</label>
-            <input type="text" name="roaster" ref={node => {input = node}} />
+            <input type="text" name="roaster" ref={node => {roaster = node}} />
           </Form.Field>
           <Form.Field>
             <label>Description</label>
-            <TextArea type="text" name="roaster" ref={node => {input = node}} />
+            <TextArea type="text" name="description" ref={node => {description = node}} />
           </Form.Field>
         </Card.Content>
         <Card.Content extra>
           <Button floated='right' onClick={() => {
-            addCoffee({name: input.value})
-            input.value = ''
+            addCoffee({name: name.value})
+            name.value = ''
           }}>
             Add
           </Button>
